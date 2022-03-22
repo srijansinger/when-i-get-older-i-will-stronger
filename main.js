4616d870c503a09e505d783dba714e744baf3634
srijansinger <87981737+srijansinger@users.noreply.github.com>
2022-03-22 15:42:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+Webcam.set({
+    width: 400,
+    height: 350,
+
+    image_format: "png",png_quality: 90
+}); 
+camera = document.getElementById("camera");
+https://teachablemachine.withgoogle.com/models/llpqxJddm/
+Webcam.attach("camera");
